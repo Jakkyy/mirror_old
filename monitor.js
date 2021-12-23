6c320855a4c94b124ff749545bd2ba76b41733c0
XII <60398054+Jakkyy@users.noreply.github.com>
2021-12-23 01:16:29 +0100

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const clientBot = new Discord.Client();
+var tempClient = new Discord.Client();
 const fetch = require('node-fetch');
 const fs = require('fs');
 const settings = JSON.parse(fs.readFileSync("settings.json", "utf8"));
-
 
 clientBot.on("ready", () => {
     console.log("Bot sbu ready");
@@ -107,6 +107,85 @@ client.on("message", async(message) => {
     }
 })
 
+clientBot.on("message", (message) => {
+
+    if (message.author.bot) return;
+    if (!clientBot) return;
+
+    const args = message.content.trim().split(/ +/g);
+
+    if (message.content.startsWith(`${settings.prefix}setup`)) {
+
+        /*
+        token = args[1].replace('"', "");
+
+        const req = await fetch("https://discord.com/api/v9/users/@me", {
+            "headers": {
+                "accept": "*",
+                "accept-language": "it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7",
+                "authorization": "NjIxNDYwNDQ3MzU0MjkwMTg2.Ya6I7g.2OmTh8DBA8LsnQ4WEW2ItwVYHDY",
+                "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Google Chrome\";v=\"96\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-debug-options": "bugReporterEnabled",
+                "x-discord-locale": "it",
+                "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6Iml0LUlUIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzk2LjAuNDY2NC4xMTAgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6Ijk2LjAuNDY2NC4xMTAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTA4OTI0LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=="
+            },
+            "referrer": "https://discord.com/login",
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": null,
+            "method": "GET",
+            "mode": "cors",
+            "credentials": "include"
+        });
+
+        let res = await req.json();
+
+        console.log("ciao")
+        if (res.message == '401: Unauthorized') return message.channel.send("Token non valido");
+        */
+
+
+
+        //temp_client.login("NjA4MDc0NjcxNjI3MzA0OTgw.YPYEYg.J1n4CVcpU6WMrlLtRUFk9MzzD9k");
+
+
+        obj = new Array();
+        guild_id_arr = new Array();
+
+        tempClient.guilds.map(guild => {
+            obj.push(`${guild.name} **-->** ${guild.id}`);
+            guild_id_arr.push(guild.id)
+        });
+
+        if (parseInt(obj.join("").length / 2048) > 0) {
+            obj = obj.join("\n").substring(0, 2047);
+        }
+
+        const embed = new Discord.RichEmbed()
+            .setColor("#ff004c")
+            .setTitle("Fetched guilds:")
+            .setDescription(obj)
+        message.channel.send(embed);
+
+
+        id_to_change = "917792875700432917"
+        current_guild = tempClient.guilds.get(id_to_change);
+
+        //message.channel.send("Choosed -> " + current_guild.name + "\nId -> " + current_guild.id)
+
+        category_object = new Array();
+        categories = tempClient.channels.filter(channel => channel.type === "category" && channel.guild.id == id_to_change).forEach(category => {
+
+            category_object.push(`${category.name} **-->** ${category.id}`);
+        });
+
+        message.channel.send(category_object)
+    }
+})
 
 function createChannel(channel_main) {
 
@@ -115,8 +194,6 @@ function createChannel(channel_main) {
     if (clientBot) {
         server.createChannel(channel_main.name, {
             type: "text",
-
-
             /// DA CAMBIARE
             /// DA CAMBIARE
             /// DA CAMBIARE
@@ -125,7 +202,7 @@ function createChannel(channel_main) {
             /// DA CAMBIARE
             /// DA CAMBIARE
             /// DA CAMBIARE
-            parent: "919633032661569557"
+            parent: "919634484301815849"
         }).then(async channel => {
             channel.createWebhook("Captain schettino").then(async webhook => {
                 webhook.send("Nuovo canale creato");
@@ -141,7 +218,7 @@ function createChannel(channel_main) {
 
                         obj = JSON.parse(data); //now it an object
 
-                        console.log(obj)
+                        //console.log(obj)
                         obj[channel_id] = webhook.url;
 
                         fs.writeFileSync('webhooks.json', JSON.stringify(obj, null, 4), 'utf8');
@@ -290,6 +367,13 @@ async function fetchData() {
 
     return [guild_id, categories_id];
 
+}
+
+tempClient.login("mfa.jVXE2ZjR2ODp7hDtGIY_VsN9MTfW_mXicsxhe9xNc8T-UF4OMWnzHV55MJatRm0wkgo0v409jUHBRu2IBona");
+
+function loginTempToken(token) {
+    tempClient.login(token);
+    return tempClient;
 }
 
 client.login("NjA4MDc0NjcxNjI3MzA0OTgw.YPYEYg.J1n4CVcpU6WMrlLtRUFk9MzzD9k");
